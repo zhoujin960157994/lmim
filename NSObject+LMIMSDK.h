06a842e5aaee5d20465a7dd922326a4e47b36e56
Ligin <3077118763@qq.com>
2021-02-07 17:14:54 +0800

    jj

---
+++ NSObject+LMIMSDK.h
@@ -0,0 +1,16 @@
+//
+//  NSObject+LMIMSDK.h
+//  lmimlib
+//
+//  Created by lm999 on 2021/2/7.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject (LMIMSDK)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NSObject+LMIMSDK.m
@@ -0,0 +1,12 @@
+//
+//  NSObject+LMIMSDK.m
+//  lmimlib
+//
+//  Created by lm999 on 2021/2/7.
+//
+
+#import "NSObject+LMIMSDK.h"
+
+@implementation NSObject (LMIMSDK)
+
+@end
